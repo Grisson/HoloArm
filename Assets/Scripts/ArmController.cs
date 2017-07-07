fc8ce9e84f7ce1895ef6eb653b88d7327e59e312
Grisson Wang <grisson@gmail.com>
2017-07-07 00:09:17 -0700

    5. Implement IManipulationHandler

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,55 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour, IManipulationHandler {
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        Debug.LogFormat("OnManipulationCanceled\r\nSource: {0}  SourceId: {1}\r\nCumulativeDelta: {2} {3} {4}",
+                eventData.InputSource,
+                eventData.SourceId,
+                eventData.CumulativeDelta.x,
+                eventData.CumulativeDelta.y,
+                eventData.CumulativeDelta.z);
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        Debug.LogFormat("OnManipulationCompleted\r\nSource: {0}  SourceId: {1}\r\nCumulativeDelta: {2} {3} {4}",
+                eventData.InputSource,
+                eventData.SourceId,
+                eventData.CumulativeDelta.x,
+                eventData.CumulativeDelta.y,
+                eventData.CumulativeDelta.z);
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        Debug.LogFormat("OnManipulationStarted\r\nSource: {0}  SourceId: {1}\r\nCumulativeDelta: {2} {3} {4}",
+                eventData.InputSource,
+                eventData.SourceId,
+                eventData.CumulativeDelta.x,
+                eventData.CumulativeDelta.y,
+                eventData.CumulativeDelta.z);
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        Debug.LogFormat("OnManipulationUpdated\r\nSource: {0}  SourceId: {1}\r\nCumulativeDelta: {2} {3} {4}",
+                    eventData.InputSource,
+                    eventData.SourceId,
+                    eventData.CumulativeDelta.x,
+                    eventData.CumulativeDelta.y,
+                    eventData.CumulativeDelta.z);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
